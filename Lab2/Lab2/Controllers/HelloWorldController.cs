f529604d39a8dc9ea00cb1a5bfbc0d8c5ec79695
Nhat2712 <nhatgaq09@gmail.com>
2025-12-04 16:37:24 +0700

    Init Project Lab2

---
+++ Lab2/Lab2/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace Lab2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+
+        public IActionResult Welcome(string name,int numTimes=1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
--- Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+619df4c85fad196a5fd2c5045f1f823fb024814d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1665bce93ab41825459c9eded747308944f8559")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
